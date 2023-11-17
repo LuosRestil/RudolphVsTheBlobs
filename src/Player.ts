08688f1e042be2784eace965cd7c594d66b63b7b
Luos Restil <luosrestil@luoss-mbp.lan>
2023-11-16 21:36:26 -0500

    02-movement

---
+++ src/Player.ts
@@ -0,0 +1,58 @@
+export class Player {
+  width: number = 50;
+  height: number = 25;
+  pos: {x: number, y: number};
+  ctx: CanvasRenderingContext2D;
+
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx;
+    this.pos = {
+      x: ctx.canvas.width / 2,
+      y: ctx.canvas.height / 2
+    }
+  }
+
+  draw() {
+    this.ctx.save();
+
+    this.ctx.translate(this.pos.x, this.pos.y);
+    // body
+    this.ctx.fillStyle = "brown";
+    this.ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+    // nose
+    this.ctx.beginPath();
+    this.ctx.arc(this.width / 2, 0, 5, 0, Math.PI * 2);
+    this.ctx.fillStyle = "red";
+    this.ctx.fill();
+    // right eye
+    this.ctx.beginPath();
+    this.ctx.arc(this.width / 4, this.height / 4, 5, 0, Math.PI * 2);
+    this.ctx.fillStyle = "white";
+    this.ctx.fill();
+    this.ctx.beginPath();
+    this.ctx.arc(this.width / 4 + 3, this.height / 4, 3, 0, Math.PI * 2);
+    this.ctx.fillStyle = "black";
+    this.ctx.fill();
+    // left eye
+    this.ctx.beginPath();
+    this.ctx.arc(this.width / 4, -this.height / 4, 5, 0, Math.PI * 2);
+    this.ctx.fillStyle = "white";
+    this.ctx.fill();
+    this.ctx.beginPath();
+    this.ctx.arc(this.width / 4 + 3, -this.height / 4, 3, 0, Math.PI * 2);
+    this.ctx.fillStyle = "black";
+    this.ctx.fill();
+    // legs
+    this.ctx.fillStyle = "brown";
+    this.ctx.fillRect(0, -this.height / 2 - 10, 5, 10);
+    this.ctx.fillRect(0, this.height / 2, 5, 10);
+    this.ctx.fillRect(-20, -this.height / 2 - 10, 5, 10);
+    this.ctx.fillRect(-20, this.height / 2, 5, 10);
+    // tail
+    this.ctx.beginPath();
+    this.ctx.arc(-this.width / 2, 0, 5, 0, Math.PI * 2);
+    this.ctx.fill();
+
+    this.ctx.restore();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,80 @@
+import { Player } from "./Player";
 import "./style.css";
 
-const canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
+const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
 canvas.width = 1280;
 canvas.height = 720;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const width = 50;
-const height = 25;
-ctx.translate(canvas.width/2, canvas.height/2);
-// body
-ctx.fillStyle = "brown";
-ctx.fillRect(-width / 2, -height / 2, width, height);
-// nose
-ctx.beginPath();
-ctx.arc(width / 2, 0, 5, 0, Math.PI * 2);
-ctx.fillStyle = "red";
-ctx.fill();
-// right eye
-ctx.beginPath();
-ctx.arc(width / 4, height / 4, 5, 0, Math.PI * 2);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.beginPath();
-ctx.arc(width / 4 + 3, height / 4, 3, 0, Math.PI * 2);
-ctx.fillStyle = "black";
-ctx.fill();
-// left eye
-ctx.beginPath();
-ctx.arc(width / 4, -height / 4, 5, 0, Math.PI * 2);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.beginPath();
-ctx.arc(width / 4 + 3, -height / 4, 3, 0, Math.PI * 2);
-ctx.fillStyle = "black";
-ctx.fill();
-// legs
-ctx.fillStyle = "brown";
-ctx.fillRect(0, -height / 2 - 10, 5, 10);
-ctx.fillRect(0, height / 2, 5, 10);
-ctx.fillRect(-20, -height / 2 - 10, 5, 10);
-ctx.fillRect(-20, height / 2, 5, 10);
-// tail
-ctx.beginPath();
-ctx.arc(-width / 2, 0, 5, 0, Math.PI * 2);
-ctx.fill();
+const keys = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
+
+document.addEventListener("keydown", (evt) => {
+  const key = evt.key;
+  switch (key) {
+    case "ArrowUp":
+    case "w":
+      keys.up = true;
+      break;
+    case "ArrowDown":
+    case "s":
+      keys.down = true;
+      break;
+    case "ArrowLeft":
+    case "a":
+      keys.left = true;
+      break;
+    case "ArrowRight":
+    case "d":
+      keys.right = true;
+      break;
+  }
+});
+
+document.addEventListener("keyup", (evt) => {
+  const key = evt.key;
+  switch (key) {
+    case "ArrowUp":
+    case "w":
+      keys.up = false;
+      break;
+    case "ArrowDown":
+    case "s":
+      keys.down = false;
+      break;
+    case "ArrowLeft":
+    case "a":
+      keys.left = false;
+      break;
+    case "ArrowRight":
+    case "d":
+      keys.right = false;
+      break;
+  }
+});
+
+let lastTime = 0;
+const speed = 200;
+
+const player = new Player(ctx);
+
+requestAnimationFrame(animate);
+
+function animate(ms: number) {
+  requestAnimationFrame(animate);
+
+  const dt = ms - lastTime;
+  const dts = dt / 1000;
+  lastTime = ms;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  if (keys.up) player.pos.y -= speed * dts;
+  if (keys.down) player.pos.y += speed * dts;
+  if (keys.left) player.pos.x -= speed * dts;
+  if (keys.right) player.pos.x += speed * dts;
+}
